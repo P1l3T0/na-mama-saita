9650805edf0265fa47c827a500a6d62023ef5c31
P1l3T0 <vgan3v@gmail.com>
2023-04-05 23:00:47 +0300

    temporary removed the blur effect

--- app.js
+++ app.js
@@ -2,32 +2,19 @@ hamburgerMenu();
 
 function hamburgerMenu() {
     const sideMenu = document.getElementById("side-menu");
-    const blurEffect = document.querySelector("#blur");
 
     document.getElementById("open-menu").addEventListener("click", function () {
         sideMenu.style.right = "0";
         sideMenu.style.transition = "500ms ease";
-
-        blurEffect.style.pointerEvents = 'auto';
-        setTimeout(() => {
-            blurEffect.classList.add("blur-overlay");
-        }, 5);
     });
 
     document.getElementById("close-menu").addEventListener("click", function () {
         sideMenu.style.right = "-210px";
         sideMenu.style.transition = "500ms ease";
-
-        blurEffect.style.pointerEvents = 'none';
-        setTimeout(() => {
-            blurEffect.classList.remove("blur-overlay");
-        }, 5);
     });
 }
-
 
 window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
-
