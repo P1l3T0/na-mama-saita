ffd34ab3cde27b40ab4106019859efa30f35726d
P1l3T0 <vgan3v@gmail.com>
2023-05-13 13:33:07 +0300

    made form it's own file

--- src/AboutPage/About.jsx
+++ src/AboutPage/About.jsx
@@ -1,3 +1,4 @@
+import { Contatcs } from "./Contavts";
 import { Teachers } from "./Teachers";
 
 export function About() {
@@ -12,40 +13,8 @@ export function About() {
                         Itaque atque voluptate expedita nemo dolor aperiam provident illum nesciunt non sed?
                     </p>
                 </div>
-
                 <Teachers />
-
-                <div className="contacts">
-                    <div className="contacts-questions">
-                        <h2>Въпроси относно начина на обучение</h2>
-                        <p>
-                            Ако имате въпроси относно дадени дисциплини, които се изучават, специалността,
-                            кандидатстването в нея или нещо друго, което сте сметнали за важно, може да пишете
-                            на имейлите ни или да напишете Вашето съобщение във формата за контакти отдясно.
-                            Ще се свържем с Вас в рамките на няколко часа или до 1 работен ден.
-                        </p>
-                    </div>
-                    <div className="contacts-form">
-                        <form
-                            action="https://formsubmit.co/vgan3v@gmail.com"
-                            method="POST"
-                        >
-                            <select name="Курс" id="person-type" required>
-                                <option value="">--Аз съм--</option>
-                                <option value="първи курс">Първи курс</option>
-                                <option value="втори курс">Втори курс</option>
-                                <option value="трети курс">Трети курс</option>
-                                <option value="първи курс магистър">Първи курс магистър</option>
-                                <option value="втори курс магистър">Втори курс магистър</option>
-                                <option value="кандидат-студент">Кандидат-сТУдент</option>
-                            </select>
-                            <input name="Име" type="text" placeholder="Вашето име" required />
-                            <input name="Имейл" type="email" placeholder="Вашият имейл" required />
-                            <textarea name="Съобщение" placeholder="Вашето съобщение" required></textarea>
-                            <button>Изпрати</button>
-                        </form>
-                    </div>
-                </div>
+                <Contatcs />
             </div >
         </>
     );
--- src/StudentsPage/Students.jsx
+++ src/StudentsPage/Students.jsx
@@ -6,7 +6,6 @@ export function Students() {
             <div className="students-main">
                 <h1 className="title">Студенти</h1>
                 <div className="students-info">
-                    <h2>Учебен план</h2>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
                         labore autem quod nobis, porro pariatur et illo voluptatem error
@@ -14,22 +13,24 @@ export function Students() {
                         suscipit voluptas itaque harum.
                     </p>
                 </div>
-                <div className="semesters">
-                    {items.map(subject =>
-                        <div key={subject.id}>
-                            <a href={subject.url} download>
-                                <h3>{subject.semester}</h3>
-                                <ul>
-                                    {subject.subjects.map((item, index) =>
-                                        <li key={index}>{item}</li>
-                                    )}
-                                </ul>
-                            </a>
-                        </div>
-                    )}
+                <div className="students-semesters">
+                    <h2>Учебен план</h2>
+                    <div className="semesters">
+                        {items.map(subject =>
+                            <div key={subject.id}>
+                                <a href={subject.url} download>
+                                    <h3>{subject.semester}</h3>
+                                    <ul>
+                                        {subject.subjects.map((item, index) =>
+                                            <li key={index}>{item}</li>
+                                        )}
+                                    </ul>
+                                </a>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
-
         </>
     );
 }
