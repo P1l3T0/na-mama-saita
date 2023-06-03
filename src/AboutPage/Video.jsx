fd91cd900beb123b83be9731f910fe31d1ad0ce7
P1l3T0 <vgan3v@gmail.com>
2023-06-03 08:11:22 +0300

    video now resizable with window width

--- src/AboutPage/Video.jsx
+++ src/AboutPage/Video.jsx
@@ -1,6 +1,21 @@
 import ReactPlayer from 'react-player';
+import { useState } from 'react';
 
 export function Video() {
+    const [width, setWidth] = useState(1280);
+    const [height, setHeight] = useState(720);
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth <= 1000) {
+            setWidth(640);
+            setHeight(360);
+        }
+        else {
+            setWidth(1280);
+            setHeight(720);
+        }
+    });
+
     return (
         <>
             <h2 className="teachers-h2">Кои сме ние?</h2>
@@ -8,6 +23,8 @@ export function Video() {
                 <ReactPlayer
                     url="https://www.youtube.com/watch?v=Nj3smRGS-LY"
                     controls muted
+                    width={width}
+                    height={height}
                 />
             </div>
         </>
